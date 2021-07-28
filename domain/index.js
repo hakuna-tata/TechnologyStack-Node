e72ddd334b92b327f0bf9485bd48520f9f0f1783
hakuna-tata <610578197@qq.com>
2021-07-28 23:50:35 +0800

    feat(domain): 利用domain做请求上下文隔离

--- domain/index.js
+++ domain/index.js
@@ -1,28 +1,51 @@
 const http = require('http');
 const domain = require('domain');
 
-var _uid = 0;
+const doRoute = (d) => {
+    console.log(process.domain === d);
 
-const doRoute = (currentDomain) => {
-    console.log(currentDomain);
-    console.log(process.domain === currentDomain);
+    const reqId = parseInt(d.req.url.substr(1));
+    d.reqId = reqId;
+    
+    setTimeout(() => {
+        if(reqId === 3){
+　　　　　　throw new Error('test error domain context')
+　　　　}
+
+　　　　d.res.end(`test success domain context: ${d.reqId}`)
+    }, (5 - reqId) * 1000)
 }
 
 const domainRequest = (req, res) => {
-    const d = domain.create();
+    let d = domain.create();
 
-    d.start =  Date.now();
-    d._uid = ++_uid;
+    d.start = Date.now();
+    d.req = req;
+    d.res = res;
 
-    // 使用 process 监听 uncaughtException 捕获也可以
-    d.on('error', (err) => {
-        console.log(err);
-        res.writeHead(500);
-        res.end("server error\n");
-    });
-
+    // 显示绑定
     d.add(req);
     d.add(res);
+
+    // 使用 uncaughtException 捕获也可以, 参考 ./globalCatch.js 文件
+    d.on('error', (err) => {
+        console.log(err);
+        // 当前上下文，并不会出现串台
+        const curDomain = process.domain;
+
+        curDomain.res.writeHead(500);
+        curDomain.res.end(`server error: ${curDomain.reqId}`);
+    });
+
+    res.once('finish', () => {
+        res.removeAllListeners('finish');
+        
+        d.remove(req);
+        d.remove(res);
+        req = null;
+        res = null;
+        d = null;
+    })
 
     d.run(() => {
         doRoute(d);
